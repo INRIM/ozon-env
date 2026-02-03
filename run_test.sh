f2325a835f19b44b136768a1a735e300a6f71ea7
Simone Gallo <simone.gallo@bmeweb.it>
2026-02-03 10:59:44 +0100

    [UPD] poetry project management

--- run_test.sh
+++ run_test.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 echo "update system"
-poetry update
+poetry install
 echo "make compose"
 docker compose  up -d --force-recreate
 echo "check code"
 poetry run black ozonenv/**/*.py
 #poetry run flake8 ozonenv/**/*.py
-pip install --upgrade -e .
 echo "run test"
 rm -rf tests/models
 time poetry run pytest --cov --cov-report=html -vv -x -s
